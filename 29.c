9e54d1772f282c5fe4e4019d2ebdeae4cb9c8081
Manan-0107 <mananshah1127@gmail.com>
2025-08-13 18:31:05 +0530

    Update 29.c

--- 29.c
+++ 29.c
@@ -3,21 +3,21 @@ int main(){
     int net_sales,gross_sales,discount;
     printf("enter the gross sales");
     scanf("%d",&net_sales);
-    printf("enter the value of discount");
-    scanf("%d",&discount);
+    
     if (gross_sales>20000){
-        discount=discount*0.15;
+        discount=gross_sales*0.15;
         net_sales=gross_sales-discount;
         printf("the net salary is %d",net_sales);
     }
     else if (gross_sales>10000){
-        discount=discount*0.1;
+        discount=gross_sales*0.1;
         net_sales=gross_sales-discount;
         printf("the net salary is %d",net_sales);
     }
     else {
-        discount=discount*0.05;
+        discount=gross_sales*0.05;
         net_sales=gross_sales-discount;
         printf("the net salary is %d",net_sales);
     }
+
 }
