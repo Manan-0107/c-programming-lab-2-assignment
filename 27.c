05b3fcc6adba1fbe3fdd5a881219c3820908b726
Manan-0107 <mananshah1127@gmail.com>
2025-08-13 18:29:08 +0530

    Update 27.c

--- 27.c
+++ 27.c
@@ -3,19 +3,16 @@ int main(){
     int net_salary,gross_salary,allowances,deduction;
     printf("enter the gross salary");
     scanf("%d",&gross_salary);
-    printf("enter the allowances");
-    scanf("%d",&allowances);
-    printf("enter the deduction");
-    scanf("%d",&deduction);
+    
     if (gross_salary>10000){
-        allowances=allowances*0.1;
-        deduction=deduction*0.03;
+        allowances=gross_salary*0.1;
+        deduction=gross_salary*0.03;
         net_salary=gross_salary+allowances-deduction;
         printf("the net salary is %d",net_salary);
     }
     else if(gross_salary>5000){
-        allowances=allowances*0.07;
-        deduction=deduction*0.02;
+        allowances=gross_salary*0.07;
+        deduction=gross_salary*0.02;
         net_salary=gross_salary+allowances-deduction;
         printf("the net salary is %d",net_salary);
     }
@@ -23,4 +20,5 @@ int main(){
         net_salary=gross_salary+allowances-deduction;
         printf("the net salary is %d",net_salary);
     }
+
 }
